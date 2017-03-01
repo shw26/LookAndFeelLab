627fea0d46520da28ebb21e88e5d9c76c3f278b4
kevin <shw26@uw.edu>
2017-02-28 16:01:03 -0800

    Initial commit

---
+++ app/src/main/java/group1/tcss450/uw/edu/lookandfeel/StylesActivity.java
@@ -0,0 +1,13 @@
+package group1.tcss450.uw.edu.lookandfeel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StylesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_styles);
+    }
+}
